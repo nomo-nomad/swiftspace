ae98bc5e79070ca3f1b73fa8681ccc26ffee0381
nomo-nomad <58949287+nomo-nomad@users.noreply.github.com>
2021-01-12 12:30:15 +0000

    read and write files in cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,49 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <array>
 #include <vector>
+#include <iterator>
+#include <valarray>
 
 using namespace std;
+
+template <typename T>
+T compound(T principal, T intRate)
+{
+  return principal * intRate;
+}
 
 int main()
 {
   vector<int> arr0;
-  vector<int> arr1;
+  valarray<int> valArr0;
 
-  arr0 = {4, 5, 6, 7};
-  for (auto num : arr0) {
+  valArr0 = {4, 5, 6, 7};
+  arr0 = {9, 7, 8, 3, 2};
+  for (auto num : valArr0)
+  {
     cout << num << endl;
+  }
+
+  array<int, 5> nums = {1, 2, 3, 5, 6};
+
+  valarray<int> newVarr = valArr0.apply([](int elem) { return elem + 2; });
+  for (auto num : newVarr)
+  {
+    cout << num << endl;
+  }
+
+  ofstream writeFile;
+  string line;
+  writeFile.open("writeFile", ios::app);
+  writeFile << "Hello there\n";
+  writeFile.close();
+
+  ifstream readFile;
+  readFile.open("writeFile");
+  while (getline(readFile, line))
+  {
+    cout << line << '\n';
   }
 }
